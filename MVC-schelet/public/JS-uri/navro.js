e721fd5fbf2d4e8470c70510c90f2593004f5769
iRebeca <birleanurebeca@yahoo.com>
2020-05-13 18:32:54 +0300

    added

---
+++ MVC-schelet/public/JS-uri/navro.js
@@ -0,0 +1,7 @@
+
+function navroll(state) {
+    if(state === true)
+        document.getElementById("main").style.marginLeft = "250px";
+    else
+        document.getElementById("main").style.marginLeft = "0px";
+}
