07d38551ca9285788bbf219731aebcae082abbaf
iRebeca <birleanurebeca@yahoo.com>
2020-05-21 18:00:39 +0300

    start dragos

---
+++ MVC-schelet/app/core/Scrapping.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Scrapping
+{
+    // am pus intuitiv parametrul ala acolo poti sa faci cu paramentru sau poti pt ficare catorgorie sa faci alta fucntie
+    // si sa verificam in produse_scrap.phtml ce categorie s-a ales si in fuctie de ce s-a ales apelam o anumita fucntie (de apelat si verificat ce s-a ales ma ocup eu
+    //tu doar sa faci cum iti e tie mai usor
+  public static function scrapping_altex($categorie){
+
+  }
+  public static function  scrapping_emag($categorie){
+
+  }
+}
