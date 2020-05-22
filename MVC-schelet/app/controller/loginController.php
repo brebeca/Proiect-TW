22e95c4ecf69af4bd36fa04504c2e35fe31aab7c
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2020-05-22 09:24:08 +0300

    Added changePassword page+googleButton+forgetPass popup

--- MVC-schelet/app/controller/loginController.php
+++ MVC-schelet/app/controller/loginController.php
@@ -1,38 +1,41 @@
 <?php
 class loginController extends Controller {
 
-    public function login($data="")
-    {
-        if ($data == "" || strpos($data, 'fail')) {
-            $this->view('login\login');
-            $this->view->render();
-        } else {
-            $this->model('loginModel');
-            $params = explode('&', $data);
-            $email = explode('=', $params[0])[1];
-            $password = explode('=', $params[1])[1];
-            $email = str_replace("%40", '@', $email);
+  public function login($data="")
+  {
+    if ($data == "" || strpos($data, 'fail')) {
+      $this->view('login\login');
+      $this->view->render();
+    } else {
+      $this->model('loginModel');
+      $params = explode('&', $data);
+      $email = explode('=', $params[0])[1];
+      $password = explode('=', $params[1])[1];
+      $email = str_replace("%40", '@', $email);
 
-            $result_of_db_search = $this->model->performLogin($password, $email);
-            if ($result_of_db_search != null) {
+      $result_of_db_search = $this->model->performLogin($password, $email);
+      if ($result_of_db_search != null) {
 
-                header("Location:/index.php?login_succes=1&user=".$result_of_db_search[0]['username']."&id=".$result_of_db_search[0]['session']);
-            } else {
-               header("Location:/login/login?login_fail=1");
-            }
-        }
-    }
-    public function google($data=''){
-        include('googleConfig.php');
-
-
-    }
-
-
-
-     public function logout($data=""){
-      session_start();
-      session_destroy();
-      header("Location:/index.php?logout=1");
+        header("Location:/index.php?login_succes=1&user=".$result_of_db_search[0]['username']."&id=".$result_of_db_search[0]['session']);
+      } else {
+       header("Location:/login/login?login_fail=1");
      }
+   }
+ }
+
+ public function google($data=''){
+  include('googleConfig.php');
+}
+
+public function logout($data=""){
+  session_start();
+  session_destroy();
+  header("Location:/index.php?logout=1");
+}
+
+public function changePassword($data=""){
+  $this->view('changePassword\changePassword');
+  $this->view->render();
+}
+
 }
