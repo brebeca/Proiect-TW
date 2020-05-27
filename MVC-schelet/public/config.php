2a5dc81dc7eb9063dd65a1ac6673a23d300507d0
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2020-05-27 16:10:52 +0300

    adaugat butonul de compara

--- MVC-schelet/public/config.php
+++ MVC-schelet/public/config.php
@@ -4,7 +4,7 @@
   define ('DB_NAME2',  'produse_emag');
   define ('DB_USER2',  'root');
   define ('DB_PASS2',  '');
-  define ('DB_USER',  'app');
-  define ('DB_PASS',  'app');
+  define ('DB_USER',  'camelia');
+  define ('DB_PASS',  'camelia');
   define('APP_ID','birleanu-CompIT-PRD-4c545f399-aad3d24d');
 ?>
