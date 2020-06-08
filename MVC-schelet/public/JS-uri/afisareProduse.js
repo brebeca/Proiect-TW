7ac8a97d73555426ebe5c1633f9beea6cc0c555f
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2020-06-08 12:57:28 +0300

    fix popup comparare detalii

--- MVC-schelet/public/JS-uri/afisareProduse.js
+++ MVC-schelet/public/JS-uri/afisareProduse.js
@@ -66,6 +66,9 @@
             return productsStr;
         }
 
+        // Get the modal
+        var modal = document.getElementById("modalProduse");
+        // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("inchide")[0];
         // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
